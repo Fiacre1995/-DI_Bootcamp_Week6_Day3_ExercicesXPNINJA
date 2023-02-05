34520a9c3e541c7ed56270a72baa25cd27b58bd9
fiacre <armeldion95@gmail.com>
2023-02-05 22:47:07 +0000

    first commit

---
+++ src/exercicexpninja/Exercice1.java
@@ -0,0 +1,25 @@
+package exercicexpninja;
+
+public class Exercice1 {
+
+	public static void main(String[] args) {}
+		// TODO Auto-generated method stub
+	    public int warOfNumber(int[] tab) {
+	        int sumOfNbrePair = 0, sumOfNbreImpair = 0;
+	        
+	        for(int i = 0; i < tab.length; i++) {
+	            if(tab[i] % 2 == 0) {
+	                sumOfNbrePair+=tab[i];
+	            }else {
+	                sumOfNbreImpair+=tab[i];
+	            }
+	        }
+	        
+	        int sumNbreDiff = (sumOfNbreImpair < sumOfNbrePair) ? sumOfNbrePair - sumOfNbreImpair : sumOfNbreImpair - sumOfNbrePair;
+	        
+	        return sumNbreDiff;
+	    
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HP
+ *
+ */
+module DI_Boocamp_Week6_Day3_ExercicesXPNINJA {
+}
